ca322cf26b71659df92d04e3185d431e2fbf6bc4
Shin <Shin@Shin-PC>
2020-06-17 00:45:12 +0900

    commit branch dev

--- HelloGit/src/Myhello.java
+++ HelloGit/src/Myhello.java
@@ -1,6 +1,8 @@
 
 public class Myhello {
 	public static void sayHello() {
-		System.out.println("hello");
+		System.out.println("hello1");
+		System.out.println("hello2");
+		System.out.println("hello3");
 	}
 }
