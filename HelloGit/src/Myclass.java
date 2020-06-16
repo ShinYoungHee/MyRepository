3b656255e632072386c4d1464600db78c8fdebbd
Shin <Shin@Shin-PC>
2020-06-17 00:38:34 +0900

    2nd version

--- HelloGit/src/Myclass.java
+++ HelloGit/src/Myclass.java
@@ -3,5 +3,6 @@ public class Myclass {
 	public static void main(String args[]) {
 		System.out.println("Hello Git!");
 		System.out.println("My name is Shin Young Hee");
+		new Myhello().sayHello();
 	}
 }
--- HelloGit/src/Myhello.java
+++ HelloGit/src/Myhello.java
@@ -1,6 +1,6 @@
 
 public class Myhello {
-	public static void main(String args[]) {
+	public static void sayHello() {
 		System.out.println("hello");
 	}
 }
